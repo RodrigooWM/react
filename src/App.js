e905071fbd5d18707ff93148eadd1ba6fbdb8aae
unknown <rodrigoaraujo2802@gmail.com>
2024-04-09 21:30:30 +0100

    aula 5 (tô perdido)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import './App.css'
 import Ola from './components/Ola'
-
-function App() {
-  const name = 'É o Lion, não tem jeito.'.toUpperCase()
-  function sum(a,b){
-    return a + b
-  }
-
-  const url = 'https://s2-techtudo.glbimg.com/CDCDKUhS0FMmWH6daMavnixT6cg=/0x0:1024x609/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/c/u/15eppqSmeTdHkoAKM0Uw/dall-e-2.jpg'
-
+import DigaMeuNome from './components/DigaMeuNome'
+import Pessoa from'./components/Pessoa'
+export function App() {
+  let nome ="Anubis"
   return (
-    <div className="App">
-    <h1>Frase do dia: {name} </h1>
-    <p>Soma de 3 e 5 é {sum(3,5)}</p>
-    <p>Não tem não tem!</p>
-    <img src={url} alt="uma imagem"></img>
-    <Ola />
-    
+    <div className="App">  
+      <Ola />
+      <DigaMeuNome nome="A Adamastor" />
+      <DigaMeuNome nome="B Braulino" />
+      <DigaMeuNome nome = {nome} />
+      <Pessoa nome = "Tinga" idade = "36" profissao="Não te interessa" foto="https://ih1.redbubble.net/image.5287552061.6571/bg,f8f8f8-flat,200x,070,f-pad,150x150,f8f8f8.jpg"/>
+
+      
     </div>
   );
 }
-
-export default App;
+export default App
---
+++ src/components/DigaMeuNome.js
@@ -0,0 +1,8 @@
+function DigaMeuNome(props){
+    return (       
+        <div>
+            <p>Um nome com {props.nome}</p>
+        </div>
+    )
+}
+export default DigaMeuNome
