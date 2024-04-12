09be1925b9c84a2bf9f04dfdf87bae1f9910de68
unknown <rodrigoaraujo2802@gmail.com>
2024-04-12 20:29:38 +0100

    aula 8/55

--- src/components/List.js
+++ src/components/List.js
@@ -4,11 +4,15 @@ function List(){
     return(
         <>
             <h1>uma lista</h1>
-            <ul>
-                <Item marca="Ferrari"></Item>
-                <Item marca="Fiat"></Item>
+            <ol>
+                <Item marca="Ferrari" ano_lancamento={1995}></Item>
+                <Item marca="Fiat" ano_lancamento={1966}></Item>
+                <Item /*marca="Renault "*/ 
+                //Propositalmente falta informação para buscar a mensagem de erro no Item.defaultProps (Item.js).
+                ano_lancamento={1994}></Item>
+                <Item marca="Peugeot " ano_lancamento={2017}></Item>
                
-            </ul>
+            </ol>
         </>
     )
 
